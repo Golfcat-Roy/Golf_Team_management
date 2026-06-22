4834390146545e58bc5b3e89b2e7358017c5493b
roychiu <focuschiu@gmail.com>
2026-06-22 14:52:46 +0800

    Final build fix for web deployment 3

--- webApp/src/jsMain/kotlin/org/golfcat/team/project/main.kt
+++ webApp/src/jsMain/kotlin/org/golfcat/team/project/main.kt
@@ -8,30 +8,40 @@ import kotlinx.browser.window
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     val liff = window.asDynamic().liff
-    val liffId = "2010382913-rCaKoQcE"
     
     if (liff == null || liff == undefined) {
         startComposeApp()
         return
     }
 
+    val liffId = "2010382913-rCaKoQcE"
+    
+    // 增加超時保護，確保就算 LIFF 失敗也能進入程式
+    val timeout = window.setTimeout({
+        startComposeApp()
+    }, 3000)
+
     liff.init(kotlin.js.json("liffId" to liffId)).then({
+        window.clearTimeout(timeout)
         if (!liff.isLoggedIn().unsafeCast<Boolean>()) {
             liff.login()
         } else {
             startComposeApp()
         }
     }, { err ->
+        window.clearTimeout(timeout)
+        console.error("LIFF Error", err)
         startComposeApp()
     })
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 private fun startComposeApp() {
-    // 確保掛載到 document.body
     val body = document.body
     if (body != null) {
-        ComposeViewport(body) {
+        // 解決亂碼：確保 Canvas 渲染區域是乾淨的
+        body.innerHTML = "<div id='app-root' style='width:100%; height:100%;'></div>"
+        ComposeViewport(document.getElementById("app-root")!!) {
             App()
         }
     }
