e83d0c07d40e4f29ef462875a742c05b5619cdc9
roychiu <focuschiu@gmail.com>
2026-06-22 10:57:33 +0800

    Trigger build 6

--- sharedLogic/src/wasmJsMain/kotlin/org/golfcat/team/project/AuthManager.wasmJs.kt
+++
@@ -1,8 +0,0 @@
-package org.golfcat.team.project
-
-import org.golfcat.team.project.models.User
-
-actual suspend fun loginWithLine(): User? {
-    // Web 版本的 LINE 登入邏輯 (LIFF 或 OAuth)
-    return null
-}
--- sharedLogic/src/wasmJsMain/kotlin/org/golfcat/team/project/SessionManager.wasmJs.kt
+++
@@ -1,9 +0,0 @@
-package org.golfcat.team.project
-
-import org.golfcat.team.project.models.User
-
-actual object SessionProvider {
-    actual fun saveUser(user: User?) {}
-    actual fun loadUser(): User? = null
-    actual fun clear() {}
-}
