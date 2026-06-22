3e6d0cee6e867a7319ac93ebea47c3c57fca7d53
roychiu <focuschiu@gmail.com>
2026-06-22 11:15:10 +0800

    Trigger build 8

--- sharedLogic/src/jsMain/kotlin/org/golfcat/team/project/Platform.js.kt
+++ sharedLogic/src/jsMain/kotlin/org/golfcat/team/project/Platform.js.kt
@@ -11,9 +11,10 @@ actual fun getPlatform(): Platform = JsPlatform()
 
 class JsPlatformService : PlatformService {
     override suspend fun getLineUserId(): String? {
+        val liff = window.asDynamic().liff
         return try {
-            if (liff.isLoggedIn()) {
-                liff.getProfile().await().userId
+            if (liff != null && liff.isLoggedIn().unsafeCast<Boolean>()) {
+                liff.getProfile().await().asDynamic().userId as? String
             } else null
         } catch (e: Exception) {
             null
