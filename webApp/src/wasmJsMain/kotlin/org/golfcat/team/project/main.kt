c455606637fe869f7d27df11b6186a0c2bd1ce1a
roychiu <focuschiu@gmail.com>
2026-06-25 01:38:08 +0800

    Update build gardle 13

--- webApp/src/wasmJsMain/kotlin/org/golfcat/team/project/main.kt
+++ webApp/src/wasmJsMain/kotlin/org/golfcat/team/project/main.kt
@@ -2,15 +2,14 @@ package org.golfcat.team.project
 
 // 💡 確保 Compose Web 的 Window 啟動器有被正確匯入
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasWindow
 import org.golfcat.team.project.App
-
+import androidx.compose.web.renderComposable
 // 💡 因為 App() 通常就定義在 org.golfcat.team.project 這個 package 底下，
 // 所以通常不需要額外 import，直接呼叫即可！
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasWindow(
+    ComposeEntryPoint(
         title = "Golf Team Management",
         canvasElementId = "ComposeTarget" // 請確認 index.html 裡的 canvas id 也是這個
     ) {
