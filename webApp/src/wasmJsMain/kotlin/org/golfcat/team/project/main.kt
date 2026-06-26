8a3ed650a263037e479734949b59362aa9599a89
roychiu <focuschiu@gmail.com>
2026-06-26 09:42:58 +0800

    major complete modification 03

--- webApp/src/wasmJsMain/kotlin/org/golfcat/team/project/main.kt
+++ webApp/src/wasmJsMain/kotlin/org/golfcat/team/project/main.kt
@@ -3,10 +3,20 @@ package org.golfcat.team.project
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport(document.body!!) {
-        App()
+    try {
+        println("Starting Wasm Application...")
+        ComposeViewport(document.body!!) {
+            App()
+        }
+        println("Application started successfully.")
+    } catch (e: Throwable) {
+        // 💡 如果啟動失敗，直接彈出警告，讓我們知道錯誤訊息
+        val msg = "Startup Error: ${e.message}"
+        println(msg)
+        window.alert(msg)
     }
 }
